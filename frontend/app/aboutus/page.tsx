abcfbdff505541e6364ad08a79a0d5d73ffb77f6
Jesus-Alva <57468191+Jesus-Alva@users.noreply.github.com>
2026-06-10 23:07:41 -0600

    Production: Estructura basica para pagina de nosotros

---
+++ frontend/app/aboutus/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const Page: React.FC = () => {
+    return (
+        <section className="min-h-screen">
+            <span>Sobre Nosotros</span>
+        </section>
+    )
+}
+
+export default Page;
