e82b8d79a37589f3b28eaa8001adca9c8d9a643f
Jesus-Alva <57468191+Jesus-Alva@users.noreply.github.com>
2026-08-11 22:22:19 -0600

    v1.12.5 Backend: Se agrega el ENDPOINT al Swagger de FastAPI

--- backend/app/api/route/api.py
+++ backend/app/api/route/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoint import auth, users, spaces, spaces_images, images, celebrations
+from app.api.v1.endpoint import auth, users, spaces, spaces_images, images, celebrations, locations
 
 api_router = APIRouter()
 
@@ -9,3 +9,4 @@ api_router.include_router(spaces.router, prefix="/spaces", tags=["Spaces"])
 api_router.include_router(spaces_images.router, prefix="/spaces", tags=["Spaces Images"])
 api_router.include_router(images.router, prefix="/images", tags=["Images"])
 api_router.include_router(celebrations.router, prefix="/celebrations", tags=["Celebrations"])
+api_router.include_router(locations.router, prefix="/locations", tags=["Locations"])
