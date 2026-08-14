a053f7e437de2d63e618445db4bbba7a71f291bc
Jesus-Alva <57468191+Jesus-Alva@users.noreply.github.com>
2026-08-13 23:39:38 -0600

    v1.16.3 Backend: Se agregan las rutas de los ENDPOINTs a SWAGGER

--- backend/app/api/route/api.py
+++ backend/app/api/route/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoint import auth, users, spaces, spaces_images, images, celebrations, locations, team_members, banners, faqs
+from app.api.v1.endpoint import auth, users, spaces, spaces_images, images, celebrations, locations, team_members, banners, faqs, packages
 
 api_router = APIRouter()
 
@@ -13,3 +13,4 @@ api_router.include_router(locations.router, prefix="/locations", tags=["Location
 api_router.include_router(team_members.router, prefix="/team-members", tags=["Team Members"])
 api_router.include_router(banners.router, prefix="/banners", tags=["Banners"])
 api_router.include_router(faqs.router, prefix="/faqs", tags=["FAQs"])
+api_router.include_router(packages.router, prefix="/packages", tags=["Packages"])
