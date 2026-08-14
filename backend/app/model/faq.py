bbbb838abcb699bee52230d10ab0a620e4913916
Jesus-Alva <57468191+Jesus-Alva@users.noreply.github.com>
2026-08-13 22:48:15 -0600

    v1.15.0 Backend: Actualizacion de autoincrement para id en el modelo faqs

--- backend/app/model/faq.py
+++ backend/app/model/faq.py
@@ -4,7 +4,7 @@ from app.base_class import Base
 class Faq(Base):
     __tablename__ = 'faqs'
 
-    id = Column(Integer, primary_key=True, autoincrement=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     question = Column(Text, nullable=False)
     answer = Column(Text, nullable=False)
     page_id = Column(Integer, nullable=True)  # posible FK a otra tabla, no especificada
